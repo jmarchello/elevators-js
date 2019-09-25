7f16869142e2e38e6bb584567c069ce3313112d2
Josh Marchello <josh.marchello@healthsplash.com>
2019-09-24 22:03:40 -0500

    out of time, but stubbed out ElevatorSimulator

--- classes/ElevatorSimulator.js
+++ classes/ElevatorSimulator.js
@@ -1,4 +1,5 @@
-const Elevator = require('./Elevator.js');
+const Elevator = require('./Elevator');
+const ElevatorRequest = require('./ElevatorRequest');
 
 class ElevatorSimulator {
   constructor(elevatorCount=1, floorCount=1) {
@@ -9,7 +10,33 @@ class ElevatorSimulator {
     for (let i = 1; i <= elevatorCount; i++) {
       elevators[i] = new Elevator(floorCount);
     }
+
+
+    // display elevators and prompt users for a request;
+    // User may opt not to provide one.
+    // Move all elevators and prompt again.
+    // Reject requests that are invalid (letters instead of numbers, floor under 1 or over floorCount, etc..)
   }
+
+
+  processRequest(origin, destination) {
+    const request = new ElevatorRequest(origin, destination);
+    let elevatorToAnswer = this.elevators.find((elevator) => elevator.currentFloor === origin);
+
+    if (!elevatorToAnswer) {
+      // Get unoccupied elevators going the correct direct, find the closes one
+    }
+
+    if (!elevatorToAnswer) {
+      // find the nearest unoccupied elevator
+    }
+
+
+    // assign the request to elevatorToAnswer
+
+  }
+
+
 }
 
 module.exports = ElevatorSimulator;
